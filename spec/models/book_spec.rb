a781f3f1183b9b3066fc67faa97a7e07721e9435
Saimon Lovell <staysynchronize@gmail.com>
2022-06-06 20:24:00 -0400

    rspec rubocopped step 1

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
@@ -10,7 +12,7 @@ RSpec.describe Book, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_uniqueness_of(:title) }
     it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
-    
+
     it { should validate_presence_of(:isbn) }
     it { should validate_uniqueness_of(:isbn) }
     it { should validate_length_of(:isbn).is_at_least(8) }
