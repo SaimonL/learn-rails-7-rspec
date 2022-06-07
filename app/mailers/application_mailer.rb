3f8873e85e2c1f93958bfcb031d3a1894fecdeb0
Saimon Lovell <staysynchronize@gmail.com>
2022-06-06 20:20:59 -0400

    mailer rubocopped

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
   append_view_path Rails.root.join('app', 'views', 'emails')
   default from: 'from@example.com'
--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Sends welcome email to new users
 class WelcomeMailer < ApplicationMailer
   def new_user(name)
     @name = name
