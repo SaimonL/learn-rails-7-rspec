0adf1f57eee05a6c4204793347bd20f56746f012
Saimon Lovell <staysynchronize@gmail.com>
2022-06-06 20:25:52 -0400

    rspec rubocopped step 2

--- spec/routing/books_routing_spec.rb
+++ spec/routing/books_routing_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :routing do
@@ -17,7 +19,6 @@ RSpec.describe BooksController, type: :routing do
     it 'routes to #edit' do
       expect(get: '/books/1/edit').to route_to('books#edit', id: '1')
     end
-
 
     it 'routes to #create' do
       expect(post: '/books').to route_to('books#create')
